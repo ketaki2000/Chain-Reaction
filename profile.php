d797cefba7d2b710a67079623a28e3993098b00a
mounilKshah <mounilshah271@gmail.com>
2020-11-18 20:43:52 +0530

    Matches table created and displayed, UI incomplete

--- profile.php
+++ profile.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 		exit;
 }
 $id=$_SESSION["id"];
+$username=$_SESSION["username"]
 /*echo $id;
 $idSQL="select receiver_id from follow where sender_id='$id' " ;
 $rs = mysql_query($idSQL);
@@ -45,48 +46,48 @@ while($ids){
 		<script src="js/jquery-1.10.2.min.js"></script>
 		<script src="js/modernizr.js"></script>
 <script>function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.innerHTML === "+ Follow") {
-    x.innerHTML = "Following";
-  } else {
-    x.innerHTML = "+ Follow";
-  }
+	var x = document.getElementById("myDIV");
+	if (x.innerHTML === "+ Follow") {
+		x.innerHTML = "Following";
+	} else {
+		x.innerHTML = "+ Follow";
+	}
 }</script>
-		<style>
+	<style>
 			.card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  width: 40%;
+	box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+	transition: 0.3s;
+	width: 40%;
 }
 
 .card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+	box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 
 }
 
 .container {
-  padding: 2px 16px;
-border-right: 4px;
+	padding: 2px 16px;
+	border-right: 4px;
 }
 			
 			table {
 				width:  100%;
-  border-collapse: separate;
-  border-spacing: 120px 0;
+	border-collapse: separate;
+	border-spacing: 120px 0;
 
 }
 img {
-    width:  150px;
-    height: 150px;
-    object-fit: cover;
+		width:  150px;
+		height: 150px;
+		object-fit: cover;
 }
 
 td {
-  padding: 10px 0;
+	padding: 10px 0;
 }
 			.padding-table-columns td
 {
-    padding:0 15px 0 0; /* Only right padding*/
+		padding:0 15px 0 0; /* Only right padding*/
 }
 			.nav-pills > li.active > a, .nav-pills > li.active > a:focus {
 					color: black;
@@ -130,8 +131,7 @@ td {
 														<li><a class="show-1 homebutton" href="#" data-toggle="tooltip" data-original-title="Homepage"><i class="fa fa-home"></i></a></li>
 														<li><a class="show-2" href="#" data-toggle="tooltip" data-original-title="Matches"><i class="fa fa-group"></i></a></li>
 														<li><a href="game.php" target="_blank" data-toggle="tooltip" data-original-title="Play now!!"><i class="fa fa-gamepad"></i></a></li>
-																						<li><a href="reset-password.php" data-toggle="tooltip" data-original-title="Reset Password"><i class="fa fa-refresh "></i></a></li>
-
+														<li><a href="reset-password.php" data-toggle="tooltip" data-original-title="Reset Password"><i class="fa fa-refresh "></i></a></li>
 														<li><a href="logout.php" data-toggle="tooltip" data-original-title="Log out"><i class="fa fa-sign-out"></i></a></li>
 												</ul>
 										</div> <!-- /.main-menu -->
@@ -144,22 +144,21 @@ td {
 												<div class="col-md-12">
 													<div style="text-align: center;">
 														<?php
-$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-														 $sql4 = "SELECT image,username from users where id='$id'  ";
+															$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
+															// Check connection
+															if ($conn->connect_error) {
+																die("Connection failed: " . $conn->connect_error);
+															}
+															$sql4 = "SELECT image,username from users where id='$id'  ";
 															$result4 = $conn->query($sql4);
 															if ($result4->num_rows > 0){
 																$row = $result4->fetch_assoc();
 																echo "
-															  <img src=".$row["image"]." float:none  style='width:150px'/>
-															  <div class=container >
-															    <h3><b>".$row["username"]."</b></h3> 
-															  </div>
+																<img src=".$row["image"]." float:none  style='width:150px'/>
+																<div class=container >
+																	<h3><b>".$row["username"]."</b></h3> 
+																</div>
 															";
-
 															}?>
 													</div>
 
@@ -173,6 +172,7 @@ die("Connection failed: " . $conn->connect_error);
 												<div class="page-header">
 													<h2 class="page-title" >My Feed</h2>
 												</div> <!-- /.page-header -->
+
 												<div class="content-inner">
 													<div class="row">
 														<div class="container">
@@ -185,84 +185,88 @@ die("Connection failed: " . $conn->connect_error);
 																<div id="home" class="tab-pane fade in active">
 																	<h5>My dear friends.</h5>
 																	<?php
-$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$sql1 = "SELECT receiver_id from follow where sender_id='$id'  ";
-$result1 = $conn->query($sql1);
-if ($result1->num_rows > 0) {
-// output data of each row
-while($row = $result1->fetch_assoc()) {
+																	$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
+																	// Check connection
+																	if ($conn->connect_error) {
+																		die("Connection failed: " . $conn->connect_error);
+																	}
+																	$sql1 = "SELECT receiver_id from follow where sender_id='$id'  ";
+																	$result1 = $conn->query($sql1);
+																	if ($result1->num_rows > 0) {
+																		// output data of each row
+																		while($row = $result1->fetch_assoc()) {
 
-$rid= $row["receiver_id"];
-$sql2 = "SELECT * from feed where user_id='$rid'  ";
-$result2 = $conn->query($sql2);
-if ($result2->num_rows > 0) {
-// output data of each row
-while($row = $result2->fetch_assoc()) {
-//echo "<img src=".$row["image"]." alt="Avatar" style="width:150px">";
-	echo "<div class=card >
-  <img src=".$row["image"]."  style='width:150px'/>
-  <div class=container class='f' style='float:right;'>
-    <h3><b>".$row["username"]."</b></h3> 
-    <h4>".$row["status"]." against ".$row["opponent"]."</h4> 
-  </div>
-</div>";
-/*echo '<img src="'.$row["image"].'" alt="Avatar" style="width:100px" style="float:left;"/><span> '.$row["username"].'</span>';
-echo '<h1 >'.$row["username"].'</h1>' ;
-echo '<h2>'.$row["status"].' against '.$row["opponent"].'</h2>' ;*/
-echo "<hr>";
-} }
-} }
-else { echo "0 results"; }
-$conn->close();
-?>
-																</div>
-																<div id="menu1" class="tab-pane fade">
-																	<h3>Top 10 players past week</h3>
-																	<table>
-<tr>
-<th>Rank</th>
-<th>Id</th>
-<th>Username</th>
-<th>Score</th>
-<th>Follow</th>
+																			$rid= $row["receiver_id"];
+																			$sql2 = "SELECT * from feed where user_id='$rid'  ";
+																			$result2 = $conn->query($sql2);
+																			if ($result2->num_rows > 0) {
+																				// output data of each row
+																				while($row = $result2->fetch_assoc()) {
+																					//echo "<img src=".$row["image"]." alt="Avatar" style="width:150px">";
+																						echo "<div class=card >
+																						<img src=".$row["image"]."  style='width:150px'/>
+																						<div class=container class='f' style='float:right;'>
+																							<h3><b>".$row["username"]."</b></h3> 
+																							<h4>".$row["status"]." against ".$row["opponent"]."</h4> 
+																						</div>
+																					</div>";
+																					/*echo '<img src="'.$row["image"].'" alt="Avatar" style="width:100px" style="float:left;"/><span> '.$row["username"].'</span>';
+																					echo '<h1 >'.$row["username"].'</h1>' ;
+																					echo '<h2>'.$row["status"].' against '.$row["opponent"].'</h2>' ;*/
+																					echo "<hr>";
+																				} 
+																			}
+																		} 
+																	}
+																	else { 
+																		echo "0 results"; 
+																	}
+																	$conn->close();
+																	?>
+																	</div>
+																	<div id="menu1" class="tab-pane fade">
+																		<h3>Top 10 players past week</h3>
+																			<table>
+																	<tr>
+																	<th>Rank</th>
+																	<th>Id</th>
+																	<th>Username</th>
+																	<th>Score</th>
+																	<th>Follow</th>
 
-</tr>
-<?php
-$sender_id=$_SESSION['id'];
-$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$sql = "SELECT ROW_NUMBER() OVER (order by score DESC) AS rank ,id, username, score FROM leaderboard ";
-$result = $conn->query($sql);
+																	</tr>
+																	<?php
+																	$sender_id=$_SESSION['id'];
+																	$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
+																	// Check connection
+																	if ($conn->connect_error) {
+																	die("Connection failed: " . $conn->connect_error);
+																	}
+																	$sql = "SELECT ROW_NUMBER() OVER (order by score DESC) AS rank ,id, username, score FROM users WHERE score is NOT NULL AND score>0";
+																	$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-// output data of each row
-while($row = $result->fetch_assoc()) {
+																	if ($result->num_rows > 0) {
+																	// output data of each row
+																	while($row = $result->fetch_assoc()) {
 
-$sql3 = "SELECT receiver_id FROM follow where sender_id='$sender_id'  ";
-$result3 = $conn->query($sql3);
+																	$sql3 = "SELECT receiver_id FROM follow where sender_id='$sender_id'  ";
+																	$result3 = $conn->query($sql3);
 
-if ($result3->num_rows > 0){
-	//if $receiver_id=$row[id]
-	$x='Following';}
-	else{$x='+ Follow';}
-echo "<tr><td>" . $row["rank"]. "</td><td>" . $row["id"] . "</td><td>" . $row["username"] . "</td>
-<td>". $row["score"]. "</td>
-<td> <a href='follow.php?receiver_id=$row[id]&username=$row[username]'><input type='submit' id=myDIV onclick=myFunction() value='$x' class='btn'></td>
+																	if ($result3->num_rows > 0){
+																		//if $receiver_id=$row[id]
+																		$x='Following';}
+																		else{$x='+ Follow';}
+																	echo "<tr><td>" . $row["rank"]. "</td><td>" . $row["id"] . "</td><td>" . $row["username"] . "</td>
+																	<td>". $row["score"]. "</td>
+																	<td> <a href='follow.php?receiver_id=$row[id]&username=$row[username]'><input type='submit' id=myDIV onclick=myFunction() value='$x' class='btn'></td>
 
-</tr>";
-}
-echo "</table>";
-} else { echo "0 results"; }
-$conn->close();
-?>
-</table>
+																	</tr>";
+																	}
+																	echo "</table>";
+																	} else { echo "0 results"; }
+																	$conn->close();
+																	?>
+																	</table>
 																</div>
 															</div>
 														</div>
@@ -271,28 +275,107 @@ $conn->close();
 												</div><!-- /.homepage -->
 
 												<!-- matches played yet -->
+
 												<div id="menu-2" class="content">
 														<div class="page-header">
 																<h2 class="page-title" >Matches Played</h2>
 														</div> <!-- /.page-header -->
 														<div class="content-inner">
 															<div class="row">
-																<div class="container">
-																	<ul class="nav nav-pills nav-justified red">
-																		<li class="active"><a data-toggle="pill" href="#home">Vs AI bot</a></li>
-																		<li><a data-toggle="pill" href="#menu1">Vs Other Players</a></li>
-																	</ul>
 
 																	<!-- tab content -->
-																	<div class="tab-content">
-																		<div id="home" class="tab-pane fade in active">
-																			<h3>Score Against Bot</h3>
-																			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-																		</div>
-																		<div id="menu1" class="tab-pane fade">
-																			<h3>Score Against Other Players</h3>
-																			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-																		</div>
+
+<div class="container">
+  
+  <ul class="nav nav-pills nav-justified">
+
+    <li class="active"><a data-toggle="pill" href="#menu2">Vs AI bot</a></li>
+    <li><a data-toggle="pill" href="#menu3">Vs Other Players</a></li>
+  </ul>
+  
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+
+      							<h5>My dear friends.</h5>
+      							<?php
+															$conn = mysqli_connect("localhost", "root", "", "chain-reaction");
+															// Check connection
+															if ($conn->connect_error) {
+																die("Connection failed: " . $conn->connect_error);
+															}
+															// $sql0 = "SELECT username from users where id = '$id'  ";
+		      							// $result5 = $conn->query($sql0);
+
+
+		      							$sql1 = "SELECT winner_name, loser_name from matches where winner_name = '$username' OR loser_name = '$username' ";
+		      							$result1 = $conn->query($sql1);
+      								// // output data of each row
+
+      								// for($i=0;$i<$result1.length;$i++){
+	      							// 		$result1[i]['winner_name'];
+	      							// 		['loser_name'];
+	      							// 	}
+
+
+      								while($row = $result1->fetch_assoc()) {
+      									echo "winner:";
+      									echo $row["winner_name"];
+      									echo " ";
+      									echo "loser:";
+      									echo $row["loser_name"];
+      									echo "<br>";
+      								}
+
+
+      								// 	$rid= $row["receiver_id"];
+      								// 	$sql2 = "SELECT winner_ from feed where user_id='$rid'  ";
+      								// 	$result2 = $conn->query($sql2);
+      								// 	if ($result2->num_rows > 0) {
+      								// 		// output data of each row
+      								// 		while($row = $result2->fetch_assoc()) {
+      								// 			//echo "<img src=".$row["image"]." alt="Avatar" style="width:150px">";
+      								// 				echo "<div class=card >
+      								// 				<img src=".$row["image"]."  style='width:150px'/>
+      								// 				<div class=container class='f' style='float:right;'>
+      								// 					<h3><b>".$row["username"]."</b></h3> 
+      								// 					<h4>".$row["status"]." against ".$row["opponent"]."</h4> 
+      								// 				</div>
+      								// 			</div>";
+      								// 			/*echo '<img src="'.$row["image"].'" alt="Avatar" style="width:100px" style="float:left;"/><span> '.$row["username"].'</span>';
+      								// 			echo '<h1 >'.$row["username"].'</h1>' ;
+      								// 			echo '<h2>'.$row["status"].' against '.$row["opponent"].'</h2>' ;*/
+      								// 			echo "<hr>";
+      								// 		} 
+      								// 	}
+
+
+      							// else { 
+      							// 	echo "0 results"; 
+      							// }
+      							// $conn->close();
+      							?>
+
+      					</div>
+      				</div>
+
+
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Menu 3</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+</div>
+
+
+
 																	</div><!-- tab content -->
 																</div>
 															</div> <!-- /.row -->
--- src/chain-reaction.sql
+++ src/chain-reaction.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2020 at 07:56 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.13
+-- Host: localhost
+-- Generation Time: Nov 18, 2020 at 04:11 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -90,7 +89,15 @@ INSERT INTO `follow` (`follow_id`, `sender_id`, `receiver_id`) VALUES
 (29, 6, 2),
 (30, 6, 2),
 (31, 6, 2),
-(32, 6, 2);
+(32, 6, 2),
+(33, 6, 2),
+(34, 6, 9),
+(35, 6, 9),
+(36, 6, 9),
+(37, 6, 10),
+(38, 6, 10),
+(39, 16, 9),
+(40, 6, 9);
 
 -- --------------------------------------------------------
 
@@ -101,7 +108,7 @@ INSERT INTO `follow` (`follow_id`, `sender_id`, `receiver_id`) VALUES
 CREATE TABLE `leaderboard` (
   `id` int(11) NOT NULL DEFAULT 0,
   `username` varchar(50) NOT NULL,
-  `score` int(11)
+  `score` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -115,6 +122,34 @@ INSERT INTO `leaderboard` (`id`, `username`, `score`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `winner_name` varchar(50) NOT NULL,
+  `loser_name` varchar(50) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
+  `match_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matches`
+--
+
+INSERT INTO `matches` (`winner_name`, `loser_name`, `timestamp`, `match_id`) VALUES
+('xyz', 'Tadashi Hamada', '2020-11-04 19:03:21', 1),
+('Ketaki', 'xyz', '2020-11-05 19:03:46', 2),
+('Paul Charles morphy', 'xyz', '2020-11-08 19:03:59', 3),
+('xyz', 'Mounil', '2020-11-03 19:04:16', 4),
+('xyz', 'Shivs', '2020-11-13 19:04:25', 5),
+('baymax', 'xyz', '2020-11-10 19:04:39', 6),
+('AI bot', 'xyz', '2020-11-18 13:18:14', 7),
+('xyz', 'AI BOT', '2020-11-18 13:18:23', 8),
+('AI bot', 'xyz', '2020-11-02 19:07:07', 9);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -123,8 +158,8 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
-  `score` int(11),
-  `image` varchar(255)
+  `score` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -132,21 +167,21 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `score`, `image`) VALUES
-(1, 'Ketaki', '$2y$10$qR8abHUmPWJFm036wsN9X.vCIQoA7YaAaTeBeXPqglRWSonbNgLeO', '2020-10-31 17:11:38', 0, ''),
+(1, 'AI Bot', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-10-31 17:11:38', 0, ''),
 (2, 'newuser', '$2y$10$0CLwR82PbrLzC5D.xX1Q9ec6i42NZxnm/JiqYuRXTJyqHU4h4Zqrq', '2020-10-31 17:32:04', 0, ''),
 (3, 'Shivani', '$2y$10$t2Hq2mTmKpkcuvdh9VTTKO2ptomtgeMWkLN2FiILKqGBi3LklyoE6', '2020-11-11 22:33:29', 0, ''),
 (4, 'Mounil', '$2y$10$JQQz3XkItfb9jnCnjCI99..6QRoEL5rnKxr1Z/ZxvxIrEIk9UinRa', '2020-11-11 22:37:33', 0, ''),
 (5, 'ket', '$2y$10$uD.1O0VDUYmZPtP9wghlve1zbqRT3OuRQxBH.7OuEcMgY03XPurnu', '2020-11-13 14:02:42', 0, ''),
 (6, 'xyz', '$2y$10$Tnw9jgzmH.7IrAK0IVlPoez070raAySCjeb4JdZ49MtcxZu7r9amW', '2020-11-14 20:55:37', 0, 'two.jpg'),
 (7, 'dummy', '$2y$10$lwMkDy3GE9xKzy3HdXbyGeFC4HOytPhUqftCAvTyPUI2zh4.JTud2', '2020-11-17 18:50:40', 0, ''),
-(15, 'Tadashi Hamada', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:39:47', NULL, ''),
 (8, 'Paul Charles Morphy', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:42:31', 12, ''),
 (9, 'baymax', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:43:01', 8, ''),
 (10, 'Nicolas Baurbaki', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:43:19', 19, ''),
 (11, 'Shinchan Amara', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:45:34', 2, ''),
 (12, 'Gary Kasparov', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:45:34', 4, ''),
-(16, 'Black Manta', '$2y$10$vp9hhfCViAXuK2QcPc8Vl.OTEVU9ua1AJAZkwWIbWAXCI7wvdlcRK', '2020-11-17 11:47:05', NULL, '');
-
+(15, 'Tadashi Hamada', '$2y$10$gi8tA2eUe3AKDGLTNhnhv.DWThPuU5qSlt/QoPZ/XXTWqOjdzqRRO', '2020-11-17 11:39:47', NULL, ''),
+(16, 'Black Manta', '$2y$10$vp9hhfCViAXuK2QcPc8Vl.OTEVU9ua1AJAZkwWIbWAXCI7wvdlcRK', '2020-11-17 11:47:05', NULL, ''),
+(17, 'saurav', '$2y$10$418LMzTz6DX99/aoS4kVOe2anfUjTuVHLPP35La34dcX16oub7gA6', '2020-11-18 14:33:57', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -165,6 +200,12 @@ ALTER TABLE `follow`
   ADD PRIMARY KEY (`follow_id`);
 
 --
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`match_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -179,13 +220,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `follow`
 --
 ALTER TABLE `follow`
-  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
